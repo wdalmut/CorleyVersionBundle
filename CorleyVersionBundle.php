693e06a06d8ea1f6dc9490d85e042249c366d4c2
Walter Dal Mut <walter.dalmut@gmail.com>
2014-09-29 23:03:42 +0200

    First running version

---
+++ CorleyVersionBundle.php
@@ -0,0 +1,10 @@
+<?php
+namespace Corley\VersionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CorleyVersionBundle extends Bundle
+{
+
+}
+
---
+++ Tests/Command/VersionCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace Corley\VersionBundle\Tests\Command;
+
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Corley\VersionBundle\Command\VersionCommand;
+use org\bovigo\vfs\vfsStream;
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Yaml\Dumper;
+
+class VersionCommandTest extends \PHPUnit_Framework_TestCase
+{
+    private $kernel;
+    private $parser;
+    private $dumper;
+    private $root;
+
+    private function prepareCommand()
+    {
+        $this->kernel = $this->getMock('Symfony\\Component\\HttpKernel\\KernelInterface');
+        $this->parser = new Parser();
+        $this->dumper = new Dumper();
+
+        $application = new Application($this->kernel);
+        $application->add(new VersionCommand($this->parser, $this->dumper));
+
+        $this->root = vfsStream::setup('config');
+
+        $command = $application->find('corley:version');
+        $command->setRootDir(vfsStream::url('config'));
+
+        $commandTester = new CommandTester($command);
+
+        return $commandTester;
+    }
+
+    public function testBumpANewVersion()
+    {
+        $commandTester = $this->prepareCommand();
+
+        $commandTester->execute(array("version" => "x.x.x"));
+        $this->assertEquals('{ parameters: { version: { number: x.x.x } } }', file_get_contents(vfsStream::url('config/version.yml')));
+    }
+}
